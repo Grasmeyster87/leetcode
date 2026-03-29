611225a4abab6851334c0c40ab03a1672c4c41be
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-03-29 21:10:47 +0300

    Change 2573

--- JS/2573. Find the String with LCP/2573. Find the String with LCP.js
+++ JS/2573. Find the String with LCP/2573. Find the String with LCP.js
@@ -2,6 +2,49 @@
  * @param {number[][]} lcp
  * @return {string}
  */
-var findTheString = function(lcp) {
-    
+var findTheString = function (lcp) {
+    const n = lcp.length;
+    const res = new Array(n).fill('');
+    let charCode = 97; // Код символу 'a'
+
+    // 1. Жадібно заповнюємо рядок
+    for (let i = 0; i < n; i++) {
+        if (res[i] !== '') continue; // Пропускаємо, якщо символ вже призначено
+
+        // Якщо нам потрібно більше 26 символів, розв'язку не існує
+        if (charCode > 122) return ''; // Код символу 'z'
+
+        const char = String.fromCharCode(charCode++);
+        for (let j = i; j < n; j++) {
+            // Якщо lcp[i][j] > 0, то word[i] == word[j]
+            if (lcp[i][j] > 0) {
+                res[j] = char;
+            }
+        }
+    }
+
+    // Перевіряємо, чи всі індекси заповнені (захист від некоректних lcp)
+    for (let i = 0; i < n; i++) {
+        if (res[i] === '') return '';
+    }
+
+    const word = res.join('');
+
+    // 2. Валідація побудованого рядка за допомогою динамічного програмування
+    // Перевіряємо властивість: lcp[i][j] = (word[i] == word[j]) ? lcp[i+1][j+1] + 1 : 0
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            let expectedLCP = 0;
+            if (word[i] === word[j]) {
+                expectedLCP =
+                    i + 1 < n && j + 1 < n ? lcp[i + 1][j + 1] + 1 : 1;
+            }
+
+            if (lcp[i][j] !== expectedLCP) {
+                return '';
+            }
+        }
+    }
+
+    return word;
 };
